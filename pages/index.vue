2c48db8f70487c2318ecddd92c636c714cc2d813
mekanoide <omar@anaga.dev>
2024-09-09 16:31:24 +0200

    fix last visited list item

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@ useHead({
     <NuxtLink
       v-for="town in visitedTowns"
       :key="town.id"
-      class="block py-6 border-t border-1 border-dashed border-neutral-300 dark:border-neutral-700 first-of-type:border-none hover:bg-neutral-200 dark:hover:bg-neutral-800"
+      class="block px-4 py-6 border-t border-1 border-dashed border-neutral-300 dark:border-neutral-700 first-of-type:border-none hover:bg-neutral-200 dark:hover:bg-neutral-800"
       :to="`/forecast/${town.id}`">
       <span class="font-semibold">{{ town.name }}</span
       >, {{ town.province }}
